9946caeaa9d32338e92144be5767f6eaf7cbff2d
Matthew Starkey <mattstarkey@me.com>
2016-01-28 12:27:10 +0200

    Added files

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControl : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float Power;
+    public float JumpPower;
+    private float hor;
+    private float ver;
+    
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+	    hor = Input.GetAxis("Horizontal");
+	    ver = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Power * ver * Time.deltaTime);
+        transform.Translate(Vector3.right * Power * hor * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb.AddForce(Vector3.up * JumpPower);
+        }
+    }
+}
